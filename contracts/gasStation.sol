81fef334e034a7537df0002ccfd86d0a57090c22
Stefaan Ponnet <stefaan@ponnet.com>
2017-05-28 22:57:32 +0200

    documented some more + added a function to extract earned tokens.

--- contracts/gasStation.sol
+++ contracts/gasStation.sol
@@ -6,43 +6,68 @@ import '../installed_contracts/zeppelin/contracts/ownership/Ownable.sol';
 
 contract gasStation is Ownable {
 
+	// the ERC20 token that we'll use
 	IMiniMeToken token;
-	uint public exchangerate_mul;
-	uint public exchangerate_div;
+	// fixed point arithmetic to calculate token unit / wei ratio
+	uint public exchangerate_numerator;
+	uint public exchangerate_denominator;
+
+	// the gas cost in Wei to trigger the exchange function (fillMeUp)
+	// this amount will be deducted from the gas sent.
 	uint public triggercost;
 
+	// a whitelist of pubkeys that can trigger the fillMeUp function
 	mapping(address=>bool) whitelist;
 
 	// constructor
-	// declare the ERC20 token to be used.
-	function gasStation(address _tokenAddress, uint _mul, uint _div, uint _triggerCost){
+	// _tokenAddress = the ERC20 token to be used.
+	// _numerator = the numerator of the fraction for the exchange rate
+	// _denominator = the denominator of the fraction for the exchange rate
+	// _triggerCost = Wei deducted for calling the exchange function
+	function gasStation(address _tokenAddress, uint _numerator, uint _denominator, uint _triggerCost){
 		token = IMiniMeToken(_tokenAddress);
 		addAllowedCaller(msg.sender);
-		setExchangeRate(_mul,_div);
+		setExchangeRate(_numerator,_denominator);
 		setTriggerCost(_triggerCost);
 	}
 
+	// add an address to the list allowed to call the exchange function
 	function addAllowedCaller(address _caller) onlyOwner {
 		whitelist[_caller] = true;
 	}
 
-	function setExchangeRate(uint _mul,uint _div) onlyOwner {
-		exchangerate_mul = _mul;
-		exchangerate_div = _div;
+	// set the exchange rate. 
+	// Only the owner of this contract can perform this
+	function setExchangeRate(uint _numerator,uint _denominator) onlyOwner {
+		exchangerate_numerator = _numerator;
+		exchangerate_denominator = _denominator;
 	}
 
-	// triggercost is the 
+	// Update the triggercost
+	// Only the owner of this contract can perform this
 	function setTriggerCost(uint _triggerCost) onlyOwner{
 		triggercost = _triggerCost;
 	}
 
+	// empty the cash register.
+	// Only the owner of this contract can perform this
+	function emptyRegister(address _to) onlyOwner{
+		token.transfer(_to,token.balanceOf(this));
+	}
+
+	// the actual exchange function.
+	// - calculate the amount of Wei we need to send.
+	// - do some sanity checks
+	// - get _tokens amount of ERC20 tokens
+	// - send wei to _receiver
+	// 
 	function fillMeUp(address _receiver, uint _tokens){
 		// only whitelisted addresses can trigger this function
 		if (whitelist[msg.sender] != true){
 			throw;
 		}
 		// calculate how much Wei to send
-		uint amount = _tokens * exchangerate_mul / exchangerate_div;
+		uint amount = _tokens * exchangerate_numerator / exchangerate_denominator;
 		if (amount < triggercost) { throw; }
 		if (this.balance < amount) {
 			// gas tank is empty
