4ac902d826e73a4dc2f1bf4b1bb66db8725ebd22
Stefaan Ponnet <stefaan@ponnet.com>
2017-09-08 17:49:26 +0200

    alt CORS solution

--- index.js
+++ index.js
@@ -83,8 +83,21 @@ lightwallet.keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey)
 
 });
 
-app.use(cors());
-app.use(express.static('./frontend'));
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+//app.use(cors());
 app.use(bodyparser.json());
 
 function getprice(token, cb) {
